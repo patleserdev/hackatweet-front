d92b82fa3975c57de6704465085e962d4bb69a25
patleserdev <leserpat@hotmail.com>
2024-07-18 12:46:32 +0200

    sign in and sign up ok

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,22 +1,53 @@
 import styles from '../styles/Signin.module.css';
 
+import { useState } from 'react'
 
 function SignIn() {
 
-    // on click signup 
-      //get inputs
-      //fetch post user exists ?
-      // connect to the site
+  const [inputUserName, setInputUserName] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
+
+  const [errorMessage, setErrorMessage] = useState('')
+  
+
+  function SignInUser()
+  {
+        // on click 
+          // si user exist connect
+          // sinon error message
+
+        fetch('http://localhost:3000/users/signin', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({username: inputUserName, password: inputPassword }),
+        })
+        .then(response => response.json())
+        .then((data)=> {
+          console.log(data)
+          if (!data.result)
+          {
+            setErrorMessage(data.error)
+          }
+          else
+          {
+            // connect to the site
+          }
+        })
+      
+  }
 
     return (
       <div className={styles.main}>
       <h1>SignIn</h1>
           <h2>Connect to Hackatweet </h2>
           <div className={styles.form}>
-          <input type="text" id="uname" placeholder="Username"/>
-          <input type="password" id="password" placeholder="Password"/>
-          <button>Sign in</button>
+          <input type="text" id="siuname" placeholder="Username" onChange={(e) => setInputUserName(e.target.value)} value={inputUserName}/>
+          <input type="password" id="sipassword" placeholder="Password" onChange={(e) => setInputPassword(e.target.value)} value={inputPassword}/>
+          <button onClick={() => { SignInUser() }}>Sign in</button>
+          <div style={{color:'red',marginTop:'10px'}}>{errorMessage}</div>
           </div>
+          
+          
       </div>
       );
     }
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,20 +1,50 @@
 import styles from '../styles/Signup.module.css';
 
+import { useState } from 'react'
+
 function SignUp() {
 
-    // on click signup 
-      //get inputs
-      //fetch post new user
-      // connect to the site
+  const [inputFirstName, setInputFirstName] = useState('')
+  const [inputUserName, setInputUserName] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
+
+  const [errorMessage, setErrorMessage] = useState('')
+
+  function SignUpNewUser()
+  {
+      // on click signup 
+        //get inputs
+        //console.log(inputFirstName,inputUserName,inputPassword)
+        fetch('http://localhost:3000/users/signup', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ firstname: inputFirstName,username: inputUserName, password: inputPassword }),
+        })
+        .then(response => response.json())
+        .then((data)=> {
+          console.log(data)
+          if (!data.result)
+          {
+            setErrorMessage(data.error)
+          }
+          else
+          {
+            // connect to the site
+          }
+        })
+      
+  }
+
     return (
         <div className={styles.main}>
         <h1>SignUp</h1>
             <h2>Create your Hackatweet account</h2>
             <div className={styles.form}>
-            <input type="text" id="fname" placeholder="Firstname"/>
-            <input type="text" id="uname" placeholder="Username"/>
-            <input type="password" id="password" placeholder="Password"/>
-            <button>Sign up</button>
+            <input type="text" id="sufname" placeholder="Firstname" onChange={(e) => setInputFirstName(e.target.value)} value={inputFirstName}/>
+            <input type="text" id="suuname" placeholder="Username" onChange={(e) => setInputUserName(e.target.value)} value={inputUserName}/>
+            <input type="password" id="supassword" placeholder="Password" onChange={(e) => setInputPassword(e.target.value)} value={inputPassword} />
+            <button onClick={() => { SignUpNewUser() }}>Sign up</button>
+            <div style={{color:'red',marginTop:'10px'}}>{errorMessage}</div>
             </div>
         </div>
       );
