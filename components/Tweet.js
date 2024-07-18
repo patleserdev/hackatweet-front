53815d1be056aabc081b3245c20a4d25fc8ffae4
Elisa <elisafan8@gmail.com>
2024-07-18 17:34:30 +0200

    created component tweet

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,9 +1,21 @@
-import styles from '../styles/Tweet.module.css';
+import styles from "../styles/Tweet.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faHeart } from "@fortawesome/free-solid-svg-icons";
 
-function Tweet() {
+function Tweet(props) {
   return (
-    <div style={{color:'#ffffff'}}>
-      tweet
+    <div className={styles.tweet}>
+      <div className={styles.profileSection}>
+        <FontAwesomeIcon className={styles.profileIcon} icon={faUser} />
+        <span className={styles.profileFirstname}>{props.firstname}Elisa</span>
+        <span className={styles.profile}>@{props.username}elisapseudo ⸱</span>
+        <span className={styles.profile}> {props.date}A few seconds ago</span>
+      </div>
+      <div className={styles.textSection}>{props.text}blablablabla</div>
+      <div className={styles.likeSection}>
+        <FontAwesomeIcon className={styles.likeIcon} icon={faHeart} />
+        {props.likeCount}0
+      </div>
     </div>
   );
 }
