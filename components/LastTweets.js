4699ac6542f2726844d5d78913e1038bc589e8db
Elisa <elisafan8@gmail.com>
2024-07-19 09:26:35 +0200

    display latetweets components, started add/remove likes and delete option

--- components/LastTweets.js
+++ components/LastTweets.js
@@ -1,13 +1,42 @@
-import styles from '../styles/Lasttweets.module.css';
-import Tweet from './Tweet.js'
-
+import { useEffect, useState } from "react";
+import styles from "../styles/Lasttweets.module.css";
+import Tweet from "./Tweet.js";
+import moment from "moment";
 
 function LastTweets() {
-  return (
-    <div>
-      <Tweet/>
-    </div>
-  );
+  const [tweetData, setTweetData] = useState([]);
+  console.log(tweetData);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/tweets/")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        const formattedData = data.result.map((e) => {
+          return {
+            tweet_id: e.tweet_id,
+            firstname: e.authorFirstname,
+            username: e.authorUsername,
+            date: moment(e.date).startOf("minute").fromNow(),
+            text: e.text,
+            likeCount: e.likeCount,
+          };
+        });
+        setTweetData(formattedData);
+      });
+  }, []);
+
+  // delete option on token's tweets only
+  // TO BE CONTINUED (ACCESS USER_ID FROM FETCH AND FROM STORE)
+  const tweets = tweetData.map((data, i) => {
+    const canDelete = tweetData.some((e) => {
+      return "yi";
+    });
+
+    return <Tweet key={i} {...data} />;
+  });
+
+  return <div>{tweets}</div>;
 }
 
 export default LastTweets;
--- components/Tweet.js
+++ components/Tweet.js
@@ -1,20 +1,65 @@
+import { useState } from "react";
 import styles from "../styles/Tweet.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faHeart, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function Tweet(props) {
+  const [likeCount, setLikeCount] = useState(props.likeCount);
+
+  let heartCustomStyle;
+  // const handleLikeButton = () => {
+  //   // route to add or remove like
+  //   fetch(`http://localhost:3000/tweets/${props.tweet_id}`, {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       token: token, // A CHANGER QUAND USESELECTOR() DU STORE USER
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // change icon color depending on back-end response
+  //       if (data.action === "removed") {
+  //         heartCustomStyle = { cursor: "pointer" };
+  //         setLikeCount(likeCount - 1);
+  //       } else if (data.action === "added") {
+  //         heartCustomStyle = { color: "#F81770", cursor: "pointer" };
+  //         setLikeCount(likeCount + 1);
+  //       } else {
+  //       }
+  //     });
+  // };
+
+  // const handleDeleteButton = () => {
+  //   // fetch route to delete !!!
+  // };
+
   return (
     <div className={styles.tweet}>
       <div className={styles.profileSection}>
         <FontAwesomeIcon className={styles.profileIcon} icon={faUser} />
-        <span className={styles.profileFirstname}>{props.firstname}Elisa</span>
-        <span className={styles.profile}>@{props.username}elisapseudo ⸱</span>
-        <span className={styles.profile}> {props.date}A few seconds ago</span>
+        <span className={styles.profileFirstname}>{props.firstname}</span>
+        <span className={styles.profile}>@{props.username} ⸱</span>
+        <span className={styles.profiledate}> {props.date}</span>
       </div>
-      <div className={styles.textSection}>{props.text}blablablabla</div>
+      <div className={styles.textSection}>{props.text}</div>
       <div className={styles.likeSection}>
-        <FontAwesomeIcon className={styles.likeIcon} icon={faHeart} />
-        {props.likeCount}0
+        <FontAwesomeIcon
+          onClick={() => handleLikeButton()}
+          className={styles.likeIcon}
+          style={heartCustomStyle}
+          icon={faHeart}
+        />
+        {props.likeCount}
+        {props.canDelete && (
+          <FontAwesomeIcon
+            onClick={() => {
+              handleDeleteButton();
+            }}
+            className={styles.likeIcon}
+            icon={faTrash}
+          />
+        )}
       </div>
     </div>
   );
