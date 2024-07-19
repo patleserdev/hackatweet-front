27b8e1f149cf6bc76687beeec53aa6f813ebf3b1
Elisa <elisafan8@gmail.com>
2024-07-19 16:54:41 +0200

    unlock post tweet

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,63 +1,106 @@
-import styles from '../styles/Signup.module.css';
+import styles from "../styles/Signup.module.css";
 
-import { useState } from 'react'
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUserToStore } from "../reducers/user";
+
+import { useSelector } from "react-redux";
+
+import { useRouter } from "next/router";
 
 function SignUp() {
+  const router = useRouter();
 
-  const [inputFirstName, setInputFirstName] = useState('')
-  const [inputUserName, setInputUserName] = useState('')
-  const [inputPassword, setInputPassword] = useState('')
+  const dispatch = useDispatch();
 
-  const [errorMessage, setErrorMessage] = useState('')
+  const [inputFirstName, setInputFirstName] = useState("");
+  const [inputUserName, setInputUserName] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
 
-  function SignUpNewUser()
-  {
-      // on click signup 
-        //get inputs
-        //console.log(inputFirstName,inputUserName,inputPassword)
-        fetch('http://localhost:3000/users/signup', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ firstname: inputFirstName,username: inputUserName, password: inputPassword }),
-        })
-        .then(response => response.json())
-        .then((data)=> {
-          console.log(data)
-          if (!data.result)
-          {
-            setErrorMessage(data.error)
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function SignUpNewUser() {
+    // on click signup
+    //get inputs
+    //console.log(inputFirstName,inputUserName,inputPassword)
+    fetch("http://localhost:3000/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstname: inputFirstName,
+        username: inputUserName,
+        password: inputPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (!data.result) {
+          setErrorMessage(data.error);
+        } else {
+          setErrorMessage("");
+          // connect to the site
+          if (
+            dispatch(
+              addUserToStore({
+                firstname: data.firstname,
+                username: data.username,
+                token: data.token,
+                userid: data.id,
+              })
+            )
+          ) {
+            // console.log(user)
+
+            router.push("/home");
           }
-          else
-          {
-            setErrorMessage('')
-            // connect to the site
-            dispatch(addUserToStore(data));
-            window.location.href = "/home";
-          }
-        })
-      
+        }
+      });
   }
 
-    return (
-        <div className={styles.main}>
-        
-            <h2>Create your Hackatweet account</h2>
-            <div className={styles.form}>
-            <input className={styles.input} type="text" id="sufname" placeholder="Firstname" 
-            onChange={(e) => setInputFirstName(e.target.value)} value={inputFirstName}/>
+  return (
+    <div className={styles.main}>
+      <h2>Create your Hackatweet account</h2>
+      <div className={styles.form}>
+        <input
+          className={styles.input}
+          type="text"
+          id="sufname"
+          placeholder="Firstname"
+          onChange={(e) => setInputFirstName(e.target.value)}
+          value={inputFirstName}
+        />
 
-            <input className={styles.input} type="text" id="suuname" placeholder="Username" 
-            onChange={(e) => setInputUserName(e.target.value)} value={inputUserName}/>
+        <input
+          className={styles.input}
+          type="text"
+          id="suuname"
+          placeholder="Username"
+          onChange={(e) => setInputUserName(e.target.value)}
+          value={inputUserName}
+        />
 
-            <input className={styles.input} type="password" id="supassword" placeholder="Password" 
-            onChange={(e) => setInputPassword(e.target.value)} value={inputPassword} />
+        <input
+          className={styles.input}
+          type="password"
+          id="supassword"
+          placeholder="Password"
+          onChange={(e) => setInputPassword(e.target.value)}
+          value={inputPassword}
+        />
 
-            <button className={styles.button} onClick={() => { SignUpNewUser() }}>Sign up</button>
-            <div style={{color:'red',marginTop:'10px'}}>{errorMessage}</div>
-            </div>
-        </div>
-      );
-    }
-    
-    export default SignUp;
-    
+        <button
+          className={styles.button}
+          onClick={() => {
+            SignUpNewUser();
+          }}
+        >
+          Sign up
+        </button>
+        <div style={{ color: "red", marginTop: "10px" }}>{errorMessage}</div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
--- components/Trends.js
+++ components/Trends.js
@@ -1,53 +1,65 @@
-import styles from '../styles/Trends.module.css';
-import { useState ,useEffect } from 'react';
-import Link from 'next/link'
+import styles from "../styles/Trends.module.css";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 import { useSelector } from "react-redux";
 
 function Trends() {
   const [trendsData, setTrendsData] = useState([]);
   const tweetData = useSelector((state) => state.tweets.value);
+  console.log(tweetData);
 
   useEffect(() => {
-            let formattedData=[]
-            for(let onetweet of tweetData)
-            {
-              for(let trend of onetweet.trends)
-              {
-                if (trend != '')
-                {
-                  const found=formattedData.some((element) => element.trend === trend)
-                 // console.log(found)
-                  if(!found)
-                  {
-                    formattedData.push({trend : trend, nbTweet : 1})   
-                  }
-                  // else
-                  // {
-                  //   let index=formattedData.findIndex(element => element.trend === trend)
-                  //   formattedData[index].nbTweet++
-                  //   //console.log(index)
-                  // }
-                }  
-              }
-            }
-            // ajouter le nombre de tweets concernés
+    let formattedData = [];
+    console.log(trendsData);
+    for (let onetweet of tweetData) {
+      for (let trend of onetweet.trends) {
+        if (trend != "") {
+          const found = formattedData.some(
+            (element) => element.trend === trend
+          );
+          // console.log(found)
+          if (!found) {
+            formattedData.push({ trend: trend, nbTweet: 1 });
+          }
+          // else
+          // {
+          //   let index=formattedData.findIndex(element => element.trend === trend)
+          //   formattedData[index].nbTweet++
+          //   //console.log(index)
+          // }
+        }
+      }
+    }
+    // ajouter le nombre de tweets concernés
 
-            setTrendsData(formattedData)
+    setTrendsData(formattedData);
   }, []);
 
+  // creer un tableau de tous les hashtags de tweetData
+  let hashtagsArr = [];
+  function removeDuplicates(array) {
+    return array.filter((item, index) => array.indexOf(item) === index);
+  }
+  for (let onetweet of tweetData) {
+    const hashtag = onetweet.trends;
+    hashtagsArr.push(...hashtag);
+  }
+  const newArr = removeDuplicates(hashtagsArr);
 
-  const displayTrends=trendsData.map((element, i) => 
-  <div className={styles.onetrend} key={i}>
-    <div className={styles.trend}><Link href={'/hashtag/'+element.trend} >{element.trend}</Link></div>
-    <div className={styles.nbtweets}>{element.nbTweet} Tweet(s)</div> 
-    </div>)
-  
-  return (
-    <div style={{color:'#ffffff'}}>
+  // boucle sur tweetData et sur onetweet.trends et hashtagsArr qui incrementera de
+  console.log(tweetData.trends);
+  // for (let tweetTrends of tweetData.trends)
 
-      {displayTrends}
+  const displayTrends = trendsData.map((element, i) => (
+    <div className={styles.onetrend} key={i}>
+      <div className={styles.trend}>
+        <Link href={"/hashtag/" + element.trend}>{element.trend}</Link>
+      </div>
+      <div className={styles.nbtweets}>{element.nbTweet} Tweet(s)</div>
     </div>
-  );
+  ));
+
+  return <div style={{ color: "#ffffff" }}>{displayTrends}</div>;
 }
 
 export default Trends;
